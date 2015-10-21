d274792d23c782b8ae93adc20bb688b16132970c
rtobin <ryantobin248@gmail.com>
2015-10-20 18:08:59 -1000

    add controller for rental request

--- app/controllers/cat_rental_request_controller.rb
+++ app/controllers/cat_rental_request_controller.rb
@@ -1,8 +1,4 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
-
+class CatRentalRequestsController < ApplicationController
   def new
     @cat_rental_request = CatRentalRequest.new
     @cats = Cat.all
